5bb5831de859080b42138940cd769af8637d8949
sandhyaasingh <saandhyasingh17@gmail.com>
2025-04-13 23:44:06 +0530

    commit

---
+++ Chapter 2/operators.py
@@ -0,0 +1,40 @@
+# Arithmetic operators
+a = 1
+b = 2
+c = a + b
+print(c)
+
+# Assignment operators
+a = 4-2 # Assign 4-2 in a
+print(a)
+b += 3 # increment the value of b by 3 and then assign it to b (-, *, /)
+print(b)
+
+# Comparison Operators always returns boolean value (==, <, >, <=, >=, !=)
+d = 5<4 
+print(d)
+
+# Logical operators
+# or
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False) 
+
+# and
+print("True and False is ", True and False)
+print("True and True is ", True and True)
+print("False and True is ", False and True)
+print("False and False is ", False and False)
+
+# not
+print(not(True))
+
+
+
+
+
+
+
+
+
